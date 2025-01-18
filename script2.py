629a93bdf74309af35f03a098fa17f78066b5bc8
atteloiv27 <kuznetsovav2706@gmail.com>
2025-01-18 05:03:19 +0300

    Update script2.py

--- script2.py
+++ script2.py
@@ -7,7 +7,6 @@ def calculate_sum_of_factorials(primes_path, factorial_func_path):
     
     # Загружаем код функции factorial и выполняем его
     factorial_code = joblib.load(factorial_func_path)
-    exec(factorial_code)
 
     # Вычисление факториалов параллельно
     def compute_factorial(n):
